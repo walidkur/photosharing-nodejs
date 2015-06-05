07220507912793b59f32a854eca119c75452736d
dlipeles <dlipeles@us.ibm.com>
2015-06-05 11:20:14 -0400

    Apache License

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+/* Copyright 2015 IBM Corporation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -8,7 +24,7 @@ var passport = require('passport');
 var session = require('express-session');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
 
 
 
@@ -35,7 +51,7 @@ app.use(passport.session());
 require ('./config/passport.js')(passport);
 
 app.use('/', routes);
-app.use('/users', users);
+
 
 
 // catch 404 and forward to error handler
