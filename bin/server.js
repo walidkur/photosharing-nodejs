667fb99382008081bddb8569989df9c4d747454b
Anthony Dresser <dresser@us.ibm.com>
2015-06-09 08:18:32 -0400

    Added getFeed route

---
+++ bin/server.js
@@ -0,0 +1,8 @@
+var server = {
+  callback = 'https://.mybluemix.net/callback',
+  clientSecret = '',
+  clientID = '',
+  domain = 'apps.collabservnext.com',
+};
+
+exports = server;
--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../bin/server');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/');
+});
+
+//getfeed for home page content
+router.get('/getFeed', function(req, res, next){
+  //config.server.domain is the domain name of the server (without the https or the directoy i.e example.com)
+  var url = {'https://' + config.server.domain + '/files/basic/anonymous/api/documents/feed'};
+  var headers = {'Authorization': 'Bearer ' + req.user.accessToken};
+
+  var options = {
+    url: url,
+    headers: headers
+  };
+
+  request.get(options, function(error, response, body){
+    if(error){
+      res.send(error);
+    } else {
+      res.send(body);
+    }
+  });
+
 });
 
 module.exports = router;
