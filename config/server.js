23d3249dc056d26fb300a86e91a2bb074e251218
dlipeles <dlipeles@us.ibm.com>
2015-06-10 09:35:45 -0400

    Bower/Dependencies fix

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 var server = {
 
   callback: 'https://photonode.mybluemix.net/callback',
-  clientSecret: '69f09cd26e31ee2a82bff9a53ccc655d',
+  clientSecret: '6b7c29b5a51140ef48d1370b26b8317dc56a0cf69ff0766221b3ca4d0821e0fc1e7912337bcfe6b8bb14adb89fe776bfc3ffc3819326a21560febf48e3077d7bfd4e0ad5a693655293d48d21bee7acc27003f05c5e97afe6eb029462b7affe3cae17542ac877ed696d6fad701b573b10243dc7f2849bf387e96b296268f32',
   clientID: 'app_22191313_1433876719030',
   domain: 'apps.collabservnext.com',
-  
+
 };
 
 exports = server;
