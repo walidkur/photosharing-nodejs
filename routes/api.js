bc6439237934a2b4cb2ba1ac786c218cf701c559
Anthony Dresser <dresser@us.ibm.com>
2015-06-18 10:15:29 -0400

    abstracted auth check out, fixed ibmcnxdev/photosharing-nodejs#35

--- routes/api.js
+++ routes/api.js
@@ -16,16 +16,20 @@ var parseString = require('xml2js').parseString;
 var request = require('request');
 var Busboy = require('busboy');
 
+function isAuth(req, res, next){
+  if(!req.user)
+    return res.status(401).end();
+  else
+    next();
+}
+
 // redirect to homepage
 router.get('/', function(req, res, next) {
   res.redirect('/');
 });
 
 //getfeed for home page content
-router.get('/feed', function(req, res, next){
-  if(!req.user)
-    return res.status(401).end();
-
+router.get('/feed', isAuth, function(req, res, next){
   var url;
 
   if(isEmpty(req.query.uid)){
@@ -146,9 +150,7 @@ router.get('/feed', function(req, res, next){
   });
 });
 
-router.put('/photo', function(req, res, next){
-  if(!req.user)
-    return res.status(401).end();
+router.put('/photo', isAuth, function(req, res, next){
 
   if(isEmpty(req.query.pid) || isEmpty(req.query.title)) {
     console.log("Query not found");
@@ -183,10 +185,7 @@ router.put('/photo', function(req, res, next){
 });
 
 //get photo for retrieving a photo by id
-router.get('/photo', function(req, res, next){
-
-  if(!req.user)
-    return res.status(401).end();
+router.get('/photo', isAuth, function(req, res, next){
 
   //if no id was passed, return an error code
   if(isEmpty(req.query.pid) || isEmpty(req.query.lid)){
@@ -251,9 +250,7 @@ router.get('/photo', function(req, res, next){
   }
 });
 
-router.put('/like', function(req, res, next){
-  if(!req.user)
-    return res.status(401).end();
+router.put('/like', isAuth, function(req, res, next){
 
   // check to ensure the necessary queries are included
   if(isEmpty(req.query.pid) || isEmpty(req.query.lid) || isEmpty(req.query.r)){
@@ -283,9 +280,7 @@ router.put('/like', function(req, res, next){
   }
 })
 
-router.get('/comments', function(req, res, next){
-  if(!req.user)
-    return res.status(401).end();
+router.get('/comments', isAuth, function(req, res, next){
 
   //if no id or uid was passed
   if(isEmpty(req.query.pid) || isEmpty(req.query.uid)){
@@ -353,9 +348,7 @@ router.get('/comments', function(req, res, next){
   }
 });
 
-router.post('/comments', function(req, res, next){
-  if(!req.user)
-    return res.status(401).end();
+router.post('/comments', isAuth, function(req, res, next){
 
   if(isEmpty(req.query.pid) || isEmpty(req.query.uid)){
     console.log("query not found");
@@ -413,10 +406,7 @@ router.post('/comments', function(req, res, next){
 });
 
 //upload a file
-router.post('/upload', function(req, res, next){
-  // if the user does not exist, send back forbidden
-  if(!req.user)
-    return res.status(401).end()
+router.post('/upload', isAuth, function(req, res, next){
 
   // before uploading, we must obtain a nonce, which is a handshake between
   // the api and our server to allow us to post to the server
@@ -488,10 +478,7 @@ router.post('/upload', function(req, res, next){
   });
 });
 
-router.get('/profile', function(req, res, next){
-  if(!req.user){
-    return res.status(401).end();
-  }
+router.get('/profile', isAuth, function(req, res, next){
 
   if(isEmpty(req.query.uid)){
     console.log('query not found');
